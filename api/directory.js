e88034351eeeb13830e6e91b3371e240e85dc8fd
Dave Townsend <dtownsend@oxymoronical.com>
2018-07-22 06:23:42 -0700

    Fix directory contents.

--- api/directory.js
+++ api/directory.js
@@ -11,7 +11,7 @@ class PlexDirectory extends PlexContainer {
   }
 
   static create(device, path, sourceData, data) {
-    return new PlexDirectory(device, path, sourceData, data);
+    return new PlexDirectory(device, path, sourceData, data.MediaContainer);
   }
 }
 
